ad497674dcb855ac53904db048cb979de65322ef
hsinpa <hsinpa@gmail.com>
2020-07-27 23:20:59 +0800

    Add initial docker config, and unity file
    Need to create docker file for mssql, to avoid create database everytime

--- ServerCode/src/index.ts
+++ ServerCode/src/index.ts
@@ -3,6 +3,7 @@ const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const static = require('koa-static')
 const path = require('path')
+const sql = require('mssql');
 const rootRouter = require('./routing');
 
 const Router = require('koa-router');
@@ -18,8 +19,6 @@ app.use(static(
   path.join( rootFolder,  '/views')
 ));
 
-console.log("fff");
-
 app.use(views(rootFolder + '/views', {
   map: {
     html: 'handlebars'
@@ -34,7 +33,34 @@ var server = http.Server(app.callback());
 
 rootRouter(router, rootFolder);
 
+const config = {
+  user : 'SA',
+  password : 'super_duper_password@2020',
+  database : 'testdb',
+  server : 'localhost',
+  options : {
+    enableArithAbort : true
+  }
+}
+
+const run = async() => {
+  let pool;
+  try {
+    pool = await sql.connect(config);
+    const { recordset } = await sql.query `select * from users;`;
+    console.log(recordset);
+
+  } catch(err) {
+    console.log(err);
+  } finally {
+    await pool.close();
+    console.log("Connection Closed");
+  }
+}
+
+run();
+
 //"192.168.0.86"
-server.listen(env.NODE_PORT || 8020, 'localhost', function () {
-  console.log(`Application worker ${process.pid} started...`);
-});
+// server.listen(env.NODE_PORT || 8020, 'localhost', function () {
+//   console.log(`Application worker ${process.pid} started...`);
+// });
