9bb76889fda9aa1a30613f47383c4a78282dead4
hsinpa <hsinpa@gmail.com>
2020-10-03 20:47:36 +0800

    Change the server url back to google poor computing

--- HCVSAR_UnityProject/Assets/Hsinpa/Script/Utility/StringAsset.cs
+++ HCVSAR_UnityProject/Assets/Hsinpa/Script/Utility/StringAsset.cs
@@ -319,7 +319,8 @@ namespace Expect.StaticAsset {
 
         public class Domain {
             public const string LocalHost = "http://localhost:8020/";
-            public const string TestServer = "http://ec2-52-88-40-169.us-west-2.compute.amazonaws.com:84/";
+            public const string GooglePoorServer = "http://34.82.74.32:81/";
+            public const string EC2DeprecatedServer = "http://ec2-52-88-40-169.us-west-2.compute.amazonaws.com:84/";
         }
 
         public class API {
@@ -338,7 +339,7 @@ namespace Expect.StaticAsset {
 
         public static string GetFullAPIUri(string apiUrl)
         {
-            return Domain.TestServer + apiUrl;
+            return Domain.GooglePoorServer + apiUrl;
         }
 
     }
