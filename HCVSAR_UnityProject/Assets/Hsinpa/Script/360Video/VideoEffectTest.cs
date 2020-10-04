5f534d60d26cddfcf878775464732d58d106b6c1
hsinpa <hsinpa@gmail.com>
2020-10-04 09:59:39 +0800

    Add 360 video effect

---
+++ HCVSAR_UnityProject/Assets/Hsinpa/Script/360Video/VideoEffectTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hsinpa.Video
+{
+
+    public class VideoEffectTest : MonoBehaviour
+    {
+        [SerializeField]
+        private VideoEffectCtrl videoEffect;
+
+        void Start()
+        {
+            videoEffect.SetCoverPercentAnim(0, 0.1f);
+        }
+
+
+
+    }
+}
