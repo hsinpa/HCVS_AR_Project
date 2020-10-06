d4cde62024cc251fe4c67558e1b697835128bb59
Aslan <littlemovietheater@gmail.com>
2020-10-07 01:50:41 +0800

    Modify UI

---
+++ HCVSAR_UnityProject/Assets/Joe/Scripts/Game06.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game06 : MonoBehaviour
+{
+    public GameObject RotateModel;
+
+    private float y;
+
+    void Update()
+    {
+        y += Time.deltaTime * 20;
+        RotateModel.transform.rotation = Quaternion.Euler(0, 90+ y, 0);
+    }
+}
