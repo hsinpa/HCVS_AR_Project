85bc68bcb5bf945453e6a6f669b487df72f9de0f
Aslan <littlemovietheater@gmail.com>
2020-10-22 21:39:27 +0800

    Add Star End animation

---
+++ HCVSAR_UnityProject/Assets/Aslan/Script/StarVideoController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class StarVideoController : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public GameObject videoObject;
+    public Button cameraButton;
+    [HideInInspector]
+    public bool isStarVideoEnd;
+
+    public void StartPlay()
+    {
+        int isPlayVideo = PlayerPrefs.GetInt("StarPlayVideo");
+        
+        if (isPlayVideo == 1)
+        {
+            videoObject.SetActive(false);
+        }
+        else
+        {
+            cameraButton.enabled = false;
+            videoObject.SetActive(true);
+            videoPlayer.Play();
+            videoPlayer.loopPointReached += OnMovieFinished;
+        }
+    }
+
+    void OnMovieFinished(VideoPlayer player)
+    {
+        isStarVideoEnd = true;
+        PlayerPrefs.SetInt("StarPlayVideo", 1);
+        videoObject.SetActive(false);
+        cameraButton.enabled = true;
+    }
+}
